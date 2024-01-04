52e477b18328769c7e2dd3de386ed64e3c4f810a
jaebeom <sukjb80@gmail.com>
2024-01-04 18:16:02 +0900

    일기작성 기능추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DiaryEditor onCreate={onCreate}></DiaryEditor>
+      <DiaryList diaryList={data}></DiaryList>
     </div>
   );
 }
---
+++ src/DiaryEditor.js
@@ -0,0 +1,66 @@
+import { useRef, useState } from "react";
+
+function DiaryEditor({ onCreate }) {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
+  const [state, setState] = useState({
+    author: "석재범",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+    onCreate(state.author, state.content, state.emotion);
+    setState({ ...state, content: "", emotion: 1 });
+    alert("저장 성공");
+  };
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          ref={authorInput}
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        ></input>
+      </div>
+      <div>
+        <textarea
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+          placeholder="일기를 입력하세요"
+        ></textarea>
+      </div>
+      <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+        <option value={4}>4</option>
+        <option value={5}>5</option>
+      </select>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+}
+export default DiaryEditor;
